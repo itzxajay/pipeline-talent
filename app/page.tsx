38e10f55dfa6f542eca574c6b82d8eda80bdf381
Ajay Kumar <114862222+itzxajay@users.noreply.github.com>
2024-09-27 21:17:23 +0500

    while button functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client"; // This line marks the component as a Client Component
+
+import React, { useState } from 'react';
 import { Section1 } from "./Sections/Section1";
 import { Section2 } from "./Sections/Section2";
 import { Section3 } from "./Sections/Section3";
@@ -8,12 +11,13 @@ import { Section7 } from "./Sections/Section7";
 import { Section8 } from "./Sections/Section8";
 
 export default function Home() {
+  const [scenario, setScenario] = useState("Company"); // Default scenario
+
   return (
     <div className="">
-
-      <Section1 />
+      <Section1 scenario={scenario} />
       <Section2/>
-      <Section3/>
+      <Section3 scenario={scenario} setScenario={setScenario} />
       <Section4/>
       <Section5/>
       <Section7/>
